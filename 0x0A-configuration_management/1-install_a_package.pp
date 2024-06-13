aa2e42ebf2d252393727c0eca71a5b7b249fcc83
muzijerrymuller <jerrymuller883@gmail.com>
2024-06-13 05:04:21 -0700

    pack

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 #This Puppet task will guarantee that the Flask package is installed via pip3.
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
   path    => ['/usr/bin', '/bin'],
 }
