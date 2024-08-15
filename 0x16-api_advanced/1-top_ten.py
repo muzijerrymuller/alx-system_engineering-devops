e0b39411914953831934685fbfe951384b3d7f66
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-08-14 20:48:57 -0700

    fake reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+The code prints the first 10 posts
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """
+    This titles of the first 10 porsts
+    """
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.5790.102 Safari/537.36 Edg/115.0.1901.182'
+    }
+    
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise an error for bad status codes
+
+        if response.status_code == 200:
+            results = response.json().get("data", {}).get("children", [])
+            if not results:
+                print("None")
+            else:
+                for post in results:
+                    print(post.get("data", {}).get("title"))
+        else:
+            print("None")
+
+    except requests.exceptions.HTTPError:
+        print("None")
+    except requests.exceptions.RequestException:
+        print("None")
+    except ValueError:
+        print("None")
