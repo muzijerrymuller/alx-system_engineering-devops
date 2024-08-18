c0ab4ae112046d7912b0305447e4166ea2f9937a
muzijerrymuller <jerrymuller883@gmail.com.com>
2024-08-18 07:52:33 -0700

    puppet file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Jerry Muller this file fixes a 500 error that comes from apache
 
-exec { 'Fix wordpress site':
+exec { 'Resolve Technical Problems Affecting the WordPress Website':
   command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
   provider => shell,
 }
