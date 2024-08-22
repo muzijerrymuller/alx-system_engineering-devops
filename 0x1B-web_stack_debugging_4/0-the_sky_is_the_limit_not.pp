85791ce00a8dee46e5229b5b863eca09fdd9794f
muzijerrymuller <jerrymuller883@gmail.com>
2024-08-22 07:03:41 -0700

    the sky is the limit with regards to requests

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# This script ensures that there is no failed requests on the stack
+# jerrymuller
+
+exec { 'change nginx limit':
+    command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+    provider => shell,
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,5 @@
+jerry muller
+jerrymuller883"gmail.com
+
+this is my fourth debugging project
+0x1B. Web stack debugging #4
