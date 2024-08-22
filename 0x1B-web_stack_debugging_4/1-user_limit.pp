34e8c72a106e9960b122d1cd60674051a8358d0a
muzijerrymuller <jerrymuller883@gmail.com>
2024-08-22 07:19:53 -0700

    user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,3 @@
+# Modifies the operating system configuration to enable secure and error-free login with the 'holberton' user account. 
+# Additionally, it ensures that the 'holberton' user can open files and execute commands without encountering any error messages or permission-related issues. 
+# This change facilitates smoother operations and improves the user experience by eliminating potential access or execution errors.
