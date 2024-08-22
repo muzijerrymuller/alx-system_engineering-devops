7fa1ba352d80b6ba1e19c1b7c3433ac5e6f758c3
muzijerrymuller <jerrymuller883@gmail.com>
2024-08-22 07:14:39 -0700

    tamara shawa changaya

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # This script ensures that there is no failed requests on the stack
 # jerrymuller
 
-exec { 'change nginx limit':
+exec { 'my first time Altering the limit of files that a stack cann take':
     command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
     provider => shell,
 }
